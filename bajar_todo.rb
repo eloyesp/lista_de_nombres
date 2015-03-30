4a65d383245cc9ee852034d0b31665b2e2fb13e0
Eloy Espinaco <eloyesp@gmail.com>
2015-03-30 16:08:43 -0300

    Publish output del script [closes #1].
    Uso bash para el script.

--- bajar_todo.rb
+++
@@ -1,6 +0,0 @@
-letras = ('A'..'Z').to_a << 'Ñ'
-
-letras.each do |letra|
-  system "curl http://www.sitiosargentina.com.ar/Nombres/masculinos/#{ letra }.htm |
-          parse_list.rb > #{ letra }"
-end
